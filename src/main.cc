5d67c0c784c3848a5c00bd5e2f498a5a490749b6
linzhenhua <linzhenhualzh@163.com>
2020-10-11 22:21:03 +0800

    modify somethings

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,21 @@
+#include <unistd.h>
+
 #include <iostream>
+
+#include <util/net/EventLoop.h>
+#include <util/base/Logging.h>
+#include <echo/EchoServer.h>
 
 int main(int argc, char **argv) {
   std::cout << "cmake test" << std::endl;
+
+  LOG_INFO << "pid = " << getpid();
+  base::net::EventLoop loop;
+
+  EchoServer echoServer(&loop, base::net::InetAddress(2007));
+  echoServer.start();
+
+  loop.loop();
+
   return 0;
 }
