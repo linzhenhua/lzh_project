c4a043fa79aafabdeccca6ccd2c66aa45331598f
linzhenhua <linzhenhualzh@163.com>
2023-06-03 12:17:46 +0800

    modify something

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,8 @@
 #include "echo/EchoServer.h"
-#include "workflow_test/tutorial_01-wget.h"
 
 int main(int argc, char **argv) {
-  // echo_test();
+  echo_test();
 
-  wget_test();
 
   return 0;
 }
